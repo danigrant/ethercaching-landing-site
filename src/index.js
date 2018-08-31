fa5f24e323a6f6fe38d6b223c34a36ba31f59530
dani <mayor@dani.town>
2018-08-31 23:00:13 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+let template = (
+  <div className="container">
+    <div className="section title-section">
+      <img src=""/>
+      <h1></h1>
+      <i className="material-icons">face</i>
+    </div>
+    <div className="section info1-section"></div>
+    <div className="section map-section"></div>
+    <div className="section info2-section"></div>
+    <div className="section footer-section"></div>
+  </div>
+)
+
+let appRoot = document.getElementById('app')
+
+ReactDOM.render(template, appRoot)
